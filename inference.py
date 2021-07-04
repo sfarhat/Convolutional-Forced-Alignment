grad-cam pipeline done but unexpected wrong result

inference.py
@@ -48,7 +48,7 @@ def show_activation_map(model, device, input, desired_phone_idx):
     Args:
         model (nn.Module): Network to put input into
         device (torch.device): Device (cpu or cuda) 
-        input (????): Input to be fed into model
+        input (Tensor): Input to be fed into model of shape channel x features x time
         desired_phone_idx (int): The index of the desired phoneme in the output transcript to generate the activation map with respect to 
     """
 
@@ -57,18 +57,19 @@ def show_activation_map(model, device, input, desired_phone_idx):
     # Artifically create batch dimension
     input = input.to(device).unsqueeze(0)
     # output of shape batch x time x classes
-    log_probs = model(input)
+    log_probs = model(input).squeeze(0)
 
     guessed_labels = torch.argmax(log_probs, dim=1)
 
     # guessed_indices don't requires_grad, so we index into log_probs to get target_classes
     target_classes = gcam.get_target_classes(log_probs, guessed_labels, desired_phone_idx)
-    # Squeeze out channel dimension when providing interpolation size
-    cam = gcam.generate_cam(target_classes, input.squeeze(0).shape)
+    # Squeeze out batch and channel dimensions when providing interpolation size
+    cam = gcam.generate_cam(target_classes, input.squeeze(0).squeeze(0).shape)
 
     # TODO: superimpose CAM on input
     plotted_cam = cam.squeeze(0).permute(1, 2, 0).cpu()
     plt.imshow(plotted_cam)
+    plt.savefig('cam.png')
     plt.show()
 
     # TODO: if we want to do something wih this + forced alignment, use time transformation function to go back to waveform time
@@ -134,7 +135,6 @@ def beam_search_decode(log_probs, transformer):
     # Labels come from order specified in utils.py, _ represents blank
     labels = list("_ abcdefghijklmnopqrstuvwxyz'")
 
-    # TODO: path to KenLM, alpha and beta values
     decoder = CTCBeamDecoder(
         labels,
         model_path=None,

main.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import torchaudio
 import os
 from config import hparams, DATASET_DIR
-from preprocess import LibrispeechCollator, TIMITCollator
+from preprocess import LibrispeechCollator, TIMITCollator, preprocess_single_waveform
 from utils import weights_init_unif, load_from_checkpoint, save_checkpoint, TextTransformer
 from model import ASR_1 
 from training import train
 from inference import test, show_activation_map
 from timit_utils import TIMITDataset, PhonemeTransformer
 from loss import ModifiedNLLLoss
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -68,8 +69,12 @@ def main():
     elif hparams['mode'] == 'test': 
         test(net, test_loader, criterion, device, transformer)
     elif  hparams['mode'] == 'cam':
-        # TODO: figure this how to nicely pass in single inputs
-        test_input = None
+        test_waveform, _ = torchaudio.load(hparams['sample_path'])
+        test_input = preprocess_single_waveform(test_waveform, hparams['n_mels'])
+        plt.imshow(test_input.permute(1, 2, 0))
+        plt.colorbar()
+        plt.savefig('spectrogram.png')
+        plt.show()
         show_activation_map(net, device, test_input, 1)
     else:
         raise Exception('Not a valid mode. Please choose between \'train\', \'test\', or \'cam\'.')